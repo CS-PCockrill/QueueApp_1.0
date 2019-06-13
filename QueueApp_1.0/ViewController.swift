d6dce9f01aaebd4021b7b90212cf4c67ceb8b47c
patrickC20 <pcockrill2@gmail.com>
2019-06-13 19:54:36 -0400

    Initial Commit

---
+++ QueueApp_1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QueueApp_1.0
+//
+//  Created by Patrick Cockrill on 6/13/19.
+//  Copyright © 2019 Patrick Cockrill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
